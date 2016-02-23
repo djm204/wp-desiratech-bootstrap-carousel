88109acc87d03f881f34c46f44f5a36389159ebd
David Mendez <desiratech@gmail.com>
2016-02-23 17:50:11 -0600

    pictures loading

--- public/partials/desiratech-bootstrap-photocarousel-public-display.php
+++ public/partials/desiratech-bootstrap-photocarousel-public-display.php
@@ -14,3 +14,41 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="<?= plugin_dir_url(__FILE__)?>../images/pic1.jpg" alt="Accalia">
+    </div>
+
+    <div class="item">
+      <img src="<?= plugin_dir_url(__FILE__)?>../images/pic2.png" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="<?= plugin_dir_url(__FILE__)?>../images/pic3.jpg" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="<?= plugin_dir_url(__FILE__)?>../images/pic4.jpg" alt="Flower">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
