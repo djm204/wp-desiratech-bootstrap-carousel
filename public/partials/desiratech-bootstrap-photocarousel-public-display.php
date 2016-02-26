3674d0624d9dfc58bf02c5a3a68840af2da6a4fa
David Mendez <desiratech@gmail.com>
2016-02-25 18:37:21 -0600

    slowed down slider

--- public/partials/desiratech-bootstrap-photocarousel-public-display.php
+++ public/partials/desiratech-bootstrap-photocarousel-public-display.php
@@ -14,7 +14,7 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="10000">
   <!-- Indicators -->
   <ol class="hidden carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -52,3 +52,4 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+
